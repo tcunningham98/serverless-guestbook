5f9f16b3dfd9cf9a738b9d7dfab2e4bd55bd6a5e
tcunningham98 <71617851+tcunningham98@users.noreply.github.com>
2021-11-24 17:19:16 -0800

    added api

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://8d6df552.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
